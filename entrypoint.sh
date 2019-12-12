7929d3d251ff31520d52dafb796f4ad53e5608f0
Jacob <jake@jwhite.network>
2019-12-12 17:48:17 -0500

    Trying with {}

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 echo "Running Validator"
 
 if "$INPUT_DEBUG_FLAG" == "true"; then
-    echo "html5validator  --root $INPUT_ROOT $INPUT_EXTRA"
-    echo "Files in input directory $(find $INPUT_ROOT)"
+    echo "html5validator  --root ${INPUT_ROOT} ${INPUT_EXTRA}"
+    echo "Files in input directory $(find ${INPUT_ROOT})"
 fi
 
-
-html5validator --root "$INPUT_ROOT"
+# For some reason adding the input extra causes it to error out
+html5validator "${INPUT_EXTRA}" --root "${INPUT_ROOT}"
 result=$?
 
 echo ::set-output name=result::$result

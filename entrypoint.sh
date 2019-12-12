3cb3de64db73d6b35b293769dbab8455e80e17d4
Jacob <jake@jwhite.network>
2019-12-12 15:50:34 -0500

    Change input args method

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "Running Validator"
 echo ""
 
-html5validator "$@" --root "$INPUT_ROOT"
+html5validator  --root "$INPUT_ROOT" "$INPUT_ARGS"
 result=$?
 
 echo ::set-output name=result::$result

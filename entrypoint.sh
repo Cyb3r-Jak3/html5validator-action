dc8d2966f4e6b0b11722861584c9a57669f4785f
Jacob <jake@jwhite.network>
2019-12-12 16:00:15 -0500

    Seeing if args is a taken variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "Running Validator"
 echo ""
 
-html5validator  --root "$INPUT_ROOT" "$INPUT_ARGS"
+html5validator  --root "$INPUT_ROOT" "$INPUT_AD-ARGS"
 result=$?
 
 echo ::set-output name=result::$result

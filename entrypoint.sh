f43c1305dfad9e433234bc56efb3649745da880f
Jacob <jake@jwhite.network>
2019-12-12 16:11:40 -0500

    Seeing why it is having issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 echo "Running Validator"
-echo "$INPUT_ROOT $INPUT_EXTRA"
+
+echo "html5validator  --root $INPUT_ROOT $INPUT_EXTRA"
 
 html5validator  --root "$INPUT_ROOT" "$INPUT_EXTRA"
 result=$?

8290eaac867edd1f5447a143824c5895c7f69541
Jacob <jake@jwhite.network>
2019-12-12 17:38:50 -0500

    More debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ echo "Running Validator"
 
 if "$INPUT_DEBUG_FLAG" == "true"; then
     echo "html5validator  --root $INPUT_ROOT $INPUT_EXTRA"
-    ls "$INPUT_ROOT"
+    echo "Files in inpurt directory $(find $INPUT_ROOT)"
 fi
 
 
-html5validator  --root "$INPUT_ROOT" "$INPUT_EXTRA"
+html5validator "$INPUT_EXTRA" --root "$INPUT_ROOT"
 result=$?
 
 echo ::set-output name=result::$result

fbca9be9bf9563624110fe6b3c0f622854431248
Jacob <jake@jwhite.network>
2019-12-12 13:31:16 -0500

    No CMD in this image

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Running Validator"
 echo ""
-cmd `html5validator "$INPUT_ROOT"`
+echo `html5validator "$INPUT_ROOT"`
 html5validator "$INPUT_ROOT" > output.txt
 
 echo ::set-output name=result::"$(cat output.txt && echo "${INPUT_ROOT}" )"

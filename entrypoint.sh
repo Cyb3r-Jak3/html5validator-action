b30c87cbfc08cb556546e7db80cfcf8ac7f9017e
Jacob <jake@jwhite.network>
2019-12-18 10:53:53 -0500

    Still working on the issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+set -x
 
 echo "Running Validator"
 
@@ -11,5 +11,7 @@ fi
 # For some reason adding the input extra causes it to error out
 html5validator --root "${INPUT_ROOT} ${INPUT_EXTRA}"
 result=$?
+extra=${INPUT_EXTRA}
+html5validator "$extra" --root "${INPUT_ROOT}"
 
 echo ::set-output name=result::$result

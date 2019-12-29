3501079493b871936d09e4354943c2275a61389c
Jacob <jake@jwhite.network>
2019-12-29 11:58:40 -0500

    More permissions issue troubleshooting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -x
+set -ex
 
 echo "Running Validator"
 
-if [[ -n "$INPUT_DEBUG_FLAG" ]]; then
+if uses "$INPUT_DEBUG_FLAG"; then
     echo "html5validator  --root ${INPUT_ROOT} ${INPUT_EXTRA}"
     echo "Files in input directory $(find ${INPUT_ROOT})"
 fi
 
 # For some reason adding the input extra causes it to error out
-extra=${INPUT_EXTRA}
+extra="${INPUT_EXTRA}"
 html5validator "${extra}" --root "${INPUT_ROOT}"
 result=$?
 

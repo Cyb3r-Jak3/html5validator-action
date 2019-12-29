352658efc4b458e1a3ec91d0eb350013286a0a44
Jacob <jake@jwhite.network>
2019-12-29 11:48:09 -0500

    Changed to bash instead of shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -x
 
 echo "Running Validator"
 
-if "$INPUT_DEBUG_FLAG" == "true"; then
+if [[ -n "$INPUT_DEBUG_FLAG" ]]; then
     echo "html5validator  --root ${INPUT_ROOT} ${INPUT_EXTRA}"
     echo "Files in input directory $(find ${INPUT_ROOT})"
 fi

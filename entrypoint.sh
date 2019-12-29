777dbed85fef4d38ba6cd08d3b227bda30c03f8d
Jacob <jake@jwhite.network>
2019-12-29 12:08:12 -0500

    Changed branch used

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -ex
 
 echo "Running Validator"
 
-#if uses "$INPUT_DEBUG_FLAG"; then
-#    echo "html5validator  --root ${INPUT_ROOT} ${INPUT_EXTRA}"
-#    echo "Files in input directory $(find ${INPUT_ROOT})"
-#fi
+if uses "$INPUT_DEBUG_FLAG"; then
+    echo "html5validator  --root ${INPUT_ROOT} ${INPUT_EXTRA}"
+    echo "Files in input directory $(find ${INPUT_ROOT})"
+fi
 
 # For some reason adding the input extra causes it to error out
 extra="${INPUT_EXTRA}"

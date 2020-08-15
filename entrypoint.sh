05579e7ea1541d8b615b436431f7dd524f4146ab
Cyb3r Jak3 <jake@jwhite.network>
2020-08-15 10:01:30 -0400

    So string math is weird

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,8 +19,7 @@ function main() {
     fi
 
     if uses "${INPUT_EXTRA}"; then
-        BuildARGS+=$((" " + INPUT_EXTRA))
-        #BuildARGS+=${INPUT_EXTRA}
+        BuildARGS+=" "${INPUT_EXTRA}
     fi
 
     html5validator --root "${INPUT_ROOT}" --log "${INPUT_LOG_LEVEL}" ${BuildARGS} |& tee log.log

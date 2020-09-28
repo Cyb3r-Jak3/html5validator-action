c85a3260566206e0bd7c226843fd55c3c3ccb5f8
Cyb3r Jak3 <jake@jwhite.network>
2020-09-27 22:17:50 -0400

    Working on bash

--- output_check.sh
+++ output_check.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if "$VALID_OUT" -ne 0; then
+if [[ "$VALID_OUT" -ne 0 ]]; then
     echo "Valid step failed"
     exit 1
 fi
-if "$INVALID_OUT" -ne "1"; then 
+if [[ "$INVALID_OUT" -ne "1" ]]; then 
     echo "Invalid Test failed"
     exit 1;
 fi
-if "$CONFIG_OUT" -ne 0; then 
+if [[ "$CONFIG_OUT" -ne 0 ]]; then 
     echo "Config step failed"
     exit 1
 fi
-if "$EMPTY_OUT" -ne "no config file or root path given"; then
+if [[ "$EMPTY_OUT" != "no config file or root path given" ]]; then
  echo "Empty check failed"
  exit 1; 
 fi

fc1c8e893db92e89c5579bd66c96bdcc8a2885fb
Cyb3r Jak3 <jake@jwhite.network>
2020-09-27 22:09:01 -0400

    More work on getting the output bash script to work

--- output_check.sh
+++ output_check.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if ${{ steps.valid.outputs.result }} -ne 0; then
+if "${{ steps.valid.outputs.result }}" -ne 0; then
     echo "Valid step failed"
     exit 1
 fi
-if ${{ steps.config.outputs.result }} -ne 0; then 
+if "${{ steps.config.outputs.result }}" -ne 0; then 
     echo "Config step failed"
     exit 1
 fi
-if ${{ steps.invalid.outputs.result }} -ne 1; then 
+if "${{ steps.invalid.outputs.result }}" -ne "1"; then 
     echo "Invalid Test failed"
     exit 1;
 fi

31f08168c5a7188510a29ea0a162465e3b315da0
Cyb3r Jak3 <jake@jwhite.network>
2020-09-27 22:00:07 -0400

    Output check script

---
+++ output_check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if ${{ steps.valid.outputs.result }} -ne 0; then
+    echo "Valid step failed"
+    exit 1
+fi
+if ${{ steps.config.outputs.result }} -ne 0; then 
+    echo "Config step failed"
+    exit 1
+fi
+if ${{ steps.invalid.outputs.result }} -ne 1; then 
+    echo "Invalid Test failed"
+    exit 1;
+fi
+if "${{ steps.empty.outputs.result }}" -ne "no config file or root path given"; then
+ echo "Empty check failed"
+ exit 1; 
+fi
